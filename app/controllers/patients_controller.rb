7a91169bcf39cc559c683552bf5af25ef28643fe
zdravnick <zdravnick@gmail.com>
2020-05-06 23:55:17 +0500

    Added Search form, added some styles for buttons/links

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -20,36 +20,49 @@ def create
 
 end
 
-def index
-  @patients = current_doctor.patients
-end
+  def index
+    @patients = current_doctor.patients
+    name = Patient.arel_table[:name]
+    Patient.where(name.matches("%#{params[:name]}%"))
+    if params[:name].present?
+      @patients = @patients.where(name.matches("%#{params[:name]}%"))
+      else
+      flash[:notice] = 'Нет никого, доктор'
+    end
+  end
 
   def show
     @patient = Patient.find(params[:id])
     @visit = @patient.visits
+
   end
 
-  def destroy
-    if doctor_signed_in?
-      Patient.find(params[:id]).destroy
-      respond_to do |format|
-      format.html { redirect_to patients_path, notice: 'patient was successfully destroyed.' }
-      format.json { head :no_content }
+
+
+
+  def update
+
+      @patient = Patient.find(params[:id]).update(
+        doctor: current_doctor,
+        name: params[:name],
+        birthdate: DateTime.civil(params["birthdate(1i)"].to_i,
+        params["birthdate(2i)"].to_i, params["birthdate(3i)"].to_i,
+        params["birthdate(4i)"].to_i,params["birthdate(5i)"].to_i),
+        diagnosis: params["diagnosis"],
+        description: params["description"]
+      )
+      redirect_back(fallback_location: patients_path)
+      flash[:notice] = 'Данные пациента обновлены, доктор'
     end
+
+    def destroy
+      if doctor_signed_in?
+        Patient.find(params[:id]).destroy
+        respond_to do |format|
+        format.html { redirect_to patients_path, notice: 'patient was successfully destroyed.' }
+        format.json { head :no_content }
+      end
     end
   end
 
-def update
-    @patient = Patient.find(params[:id]).update(
-      doctor: current_doctor,
-      name: params[:name],
-      birthdate: DateTime.civil(params["birthdate(1i)"].to_i,
-      params["birthdate(2i)"].to_i, params["birthdate(3i)"].to_i,
-      params["birthdate(4i)"].to_i,params["birthdate(5i)"].to_i),
-      diagnosis: params["diagnosis"],
-      description: params["description"]
-    )
-    redirect_back(fallback_location: patients_path)
-    flash[:notice] = 'Данные пациента обновлены, доктор'
-  end
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,8 +1,7 @@
 class Patient < ApplicationRecord
 
   validates :name, presence: true,
-                   length: { minimum: 2, maximum: 50 },
-                   uniqueness: true
+                   length: { minimum: 2, maximum: 50 }
   belongs_to :doctor
   has_many :visits, dependent: :destroy
 
