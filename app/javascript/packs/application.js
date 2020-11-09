6eb33ed86356e7be59d5a222b6b910d4e9bffe82
zdravnick <zdravnick@gmail.com>
2020-11-09 14:59:49 +0500

    database.yml changed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-// require("controllers")
+
 
 // import { Application } from "stimulus"
 // import { definitionsFromContext } from "stimulus/webpack-helpers"
