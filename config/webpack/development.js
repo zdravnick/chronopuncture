e9421829b984ee90e9e2915d92867eec46add604
zdravnick <zdravnick@gmail.com>
2020-11-11 19:48:32 +0500

    added clean-webpack-plugin, webpack.config is not empty, just standart

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,13 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+const CleanWebpackPlugin = require("clean-webpack-plugin")
+const path = require("path")
+const environment = require("./environment")
 
-const environment = require('./environment')
-
+environment.plugins.append(
+  "CleanWebpackPlugin",
+  new CleanWebpackPlugin(["packs"], {
+    root: path.resolve(__dirname, "../../public"),
+    verbose: true
+  })
+)
 module.exports = environment.toWebpackConfig()
