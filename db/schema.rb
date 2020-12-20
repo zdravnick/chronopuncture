6cda79f1583a0f43eba3dcce6a0796fda018add0
zdravnick <zdravnick@gmail.com>
2020-12-20 23:36:34 +0500

    added some style to ol reversed_list

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+ value too long for type character varying(255)
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
