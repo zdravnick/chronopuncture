3a0b79d05706558227483e20076208e92f63f1da
zdravnick <zdravnick@gmail.com>
2021-01-10 23:22:41 +0500

    fixed bug with current_doctor.city et. set. == nil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -59,127 +59,133 @@ class ApplicationController < ActionController::Base
   end
 
   def trunc_day_show
-    city = current_doctor.city
-    date = DateTime.current.in_time_zone(current_doctor.city.time_zone)
-    trunk = Trunk.find(trunc_day_calculation(city, date))
-    @trunk_today = trunk.id
-    @forbidden_action_by_days = trunk.forbidden_action_by_days
-    @trunk_energy = trunk.trunk_energy
+    if current_doctor
+      city = current_doctor.city
+      date = DateTime.current.in_time_zone(current_doctor.city.time_zone)
+      trunk = Trunk.find(trunc_day_calculation(city, date))
+      @trunk_today = trunk.id
+      @forbidden_action_by_days = trunk.forbidden_action_by_days
+      @trunk_energy = trunk.trunk_energy
+    end
   end
 
   def branch_day_show
-    city = current_doctor.city
-    date = DateTime.current.in_time_zone(current_doctor.city.time_zone)
-    @branch_day = Branch.find(branch_day_calculation(city, date)).id
-    case @branch_day
-      when 1
-        @forbidden_action_by_branch = 'Область глаз'
-      when 2
-        @forbidden_action_by_branch = 'Уши'
-      when 3
-        @forbidden_action_by_branch = 'Грудная клетка'
-      when 4
-        @forbidden_action_by_branch = 'Челюсти и зубы'
-      when 5
-        @forbidden_action_by_branch = 'Поясница'
-      when 6
-        @forbidden_action_by_branch = 'Руки'
-      when 7
-        @forbidden_action_by_branch = 'Область сердца'
-      when 8
-        @forbidden_action_by_branch = 'Стопы'
-      when 9
-        @forbidden_action_by_branch = 'Голова'
-      when 10
-        @forbidden_action_by_branch = 'Колени'
-      when 11
-        @forbidden_action_by_branch = 'Промежность'
-      when 12
-        @forbidden_action_by_branch = 'Передняя поверхность шеи'
-      else
-        @forbidden_action_by_branch = 'Ошибка в методе branch_day_show'
+    if current_doctor
+      city = current_doctor.city
+      date = DateTime.current.in_time_zone(current_doctor.city.time_zone)
+      @branch_day = Branch.find(branch_day_calculation(city, date)).id
+      case @branch_day
+        when 1
+          @forbidden_action_by_branch = 'Область глаз'
+        when 2
+          @forbidden_action_by_branch = 'Уши'
+        when 3
+          @forbidden_action_by_branch = 'Грудная клетка'
+        when 4
+          @forbidden_action_by_branch = 'Челюсти и зубы'
+        when 5
+          @forbidden_action_by_branch = 'Поясница'
+        when 6
+          @forbidden_action_by_branch = 'Руки'
+        when 7
+          @forbidden_action_by_branch = 'Область сердца'
+        when 8
+          @forbidden_action_by_branch = 'Стопы'
+        when 9
+          @forbidden_action_by_branch = 'Голова'
+        when 10
+          @forbidden_action_by_branch = 'Колени'
+        when 11
+          @forbidden_action_by_branch = 'Промежность'
+        when 12
+          @forbidden_action_by_branch = 'Передняя поверхность шеи'
+        else
+          @forbidden_action_by_branch = 'Ошибка в методе branch_day_show'
+      end
     end
   end
 
   def forbidden_action_60_days
-    city = current_doctor.city
-    date = DateTime.current.in_time_zone(current_doctor.city.time_zone)
-    day_num = number_of_day_calculation(city, date) % 60
-    case day_num
-      when 1, 31
-        @forbidden_action_60_days = 'Большой палец стопы'
-      when 2, 32
-        @forbidden_action_60_days = 'Наружная лодыжка'
-      when 3, 33
-        @forbidden_action_60_days = 'Внутренняя поверхность бедра'
-      when 4, 34
-        @forbidden_action_60_days = 'Поясница'
-      when 5, 35
-        @forbidden_action_60_days = 'Рот'
-      when 6, 36
-        @forbidden_action_60_days = 'Кулак(кисть)'
-      when 7, 37
-        @forbidden_action_60_days = 'Внутренняя лодыжка'
-      when 8, 38
-        @forbidden_action_60_days = 'Внутренняя поверхность запястья'
-      when 9, 39
-        @forbidden_action_60_days = 'Ягодицы и копчик'
-      when 10, 40
-        @forbidden_action_60_days = 'Спина и поясница'
-      when 11, 41
-        @forbidden_action_60_days = 'Спинка носа'
-      when 12, 42
-        @forbidden_action_60_days = 'Наружная лодыжка'
-      when 13, 43
-        @forbidden_action_60_days = 'Зубы и челюсти'
-      when 14, 44
-        @forbidden_action_60_days = 'Эпигастрий, область желудка'
-      when 15, 45
-        @forbidden_action_60_days = 'Все туловище'
-      when 16, 46
-        @forbidden_action_60_days = 'Межреберья'
-      when 17, 47
-        @forbidden_action_60_days = "Точка #{Point.find_by(name: 'Qi-chong')}"
-      when 18, 48
-        @forbidden_action_60_days = 'Внутренняя поверхность бедра'
-      when 19, 49
-        @forbidden_action_60_days = 'Стопы'
-      when 20, 50
-        @forbidden_action_60_days = 'Внутренняя лодыжка'
-      when 21, 51
-        @forbidden_action_60_days = 'Мизинцы рук'
-      when 22, 52
-        @forbidden_action_60_days = 'Наружная лодыжка'
-      when 23, 53
-        @forbidden_action_60_days = 'Стопы и область печени'
-      when 24, 54
-        @forbidden_action_60_days = 'Ян- и Инь-каналы рук'
-      when 25, 55
-        @forbidden_action_60_days = "Меридиан #{Meridian.find_by(name: 'Stomach')}"
-      when 26, 56
-        @forbidden_action_60_days = 'Грудь'
-      when 27, 57
-        @forbidden_action_60_days = 'Колени'
-      when 28, 58
-        @forbidden_action_60_days = 'Наружные половые органы'
-      when 29, 59
-        @forbidden_action_60_days = 'Задняя поверхность коленей и голеней'
-      when 30, 60
-        @forbidden_action_60_days = 'Область предплюсневых костей стопы'
-      else
-        @forbidden_action_60_days = 'Ошибка в методе forbidden_action_60_days'
-      end
-        if [29, 41].include?(day_num)
-          @forbidden_acu_day_60_days = 'Не лучший день для мужчин по 60-дневному циклу'
-        elsif [22, 51, 52].include?(day_num)
-          @forbidden_acu_day_60_days = 'Не лучший день для женщин по 60-дневному циклу'
-        elsif [8, 42, 43, 44].include?(day_num)
-          @forbidden_acu_day_60_days = 'Не лучший день для мужчин и женщин по 60-дневному циклу'
+    if current_doctor
+      city = current_doctor.city
+      date = DateTime.current.in_time_zone(current_doctor.city.time_zone)
+      day_num = number_of_day_calculation(city, date) % 60
+      case day_num
+        when 1, 31
+          @forbidden_action_60_days = 'Большой палец стопы'
+        when 2, 32
+          @forbidden_action_60_days = 'Наружная лодыжка'
+        when 3, 33
+          @forbidden_action_60_days = 'Внутренняя поверхность бедра'
+        when 4, 34
+          @forbidden_action_60_days = 'Поясница'
+        when 5, 35
+          @forbidden_action_60_days = 'Рот'
+        when 6, 36
+          @forbidden_action_60_days = 'Кулак(кисть)'
+        when 7, 37
+          @forbidden_action_60_days = 'Внутренняя лодыжка'
+        when 8, 38
+          @forbidden_action_60_days = 'Внутренняя поверхность запястья'
+        when 9, 39
+          @forbidden_action_60_days = 'Ягодицы и копчик'
+        when 10, 40
+          @forbidden_action_60_days = 'Спина и поясница'
+        when 11, 41
+          @forbidden_action_60_days = 'Спинка носа'
+        when 12, 42
+          @forbidden_action_60_days = 'Наружная лодыжка'
+        when 13, 43
+          @forbidden_action_60_days = 'Зубы и челюсти'
+        when 14, 44
+          @forbidden_action_60_days = 'Эпигастрий, область желудка'
+        when 15, 45
+          @forbidden_action_60_days = 'Все туловище'
+        when 16, 46
+          @forbidden_action_60_days = 'Межреберья'
+        when 17, 47
+          @forbidden_action_60_days = "Точка #{Point.find_by(name: 'Qi-chong')}"
+        when 18, 48
+          @forbidden_action_60_days = 'Внутренняя поверхность бедра'
+        when 19, 49
+          @forbidden_action_60_days = 'Стопы'
+        when 20, 50
+          @forbidden_action_60_days = 'Внутренняя лодыжка'
+        when 21, 51
+          @forbidden_action_60_days = 'Мизинцы рук'
+        when 22, 52
+          @forbidden_action_60_days = 'Наружная лодыжка'
+        when 23, 53
+          @forbidden_action_60_days = 'Стопы и область печени'
+        when 24, 54
+          @forbidden_action_60_days = 'Ян- и Инь-каналы рук'
+        when 25, 55
+          @forbidden_action_60_days = "Меридиан #{Meridian.find_by(name: 'Stomach')}"
+        when 26, 56
+          @forbidden_action_60_days = 'Грудь'
+        when 27, 57
+          @forbidden_action_60_days = 'Колени'
+        when 28, 58
+          @forbidden_action_60_days = 'Наружные половые органы'
+        when 29, 59
+          @forbidden_action_60_days = 'Задняя поверхность коленей и голеней'
+        when 30, 60
+          @forbidden_action_60_days = 'Область предплюсневых костей стопы'
         else
-          @forbidden_acu_day_60_days = 'Все хорошо, колите, доктор, колите'
+          @forbidden_action_60_days = 'Ошибка в методе forbidden_action_60_days'
         end
-    end
 
+      if [29, 41].include?(day_num)
+        @forbidden_acu_day_60_days = 'Не лучший день для мужчин по 60-дневному циклу'
+      elsif [22, 51, 52].include?(day_num)
+        @forbidden_acu_day_60_days = 'Не лучший день для женщин по 60-дневному циклу'
+      elsif [8, 42, 43, 44].include?(day_num)
+        @forbidden_acu_day_60_days = 'Не лучший день для мужчин и женщин по 60-дневному циклу'
+      else
+        @forbidden_acu_day_60_days = 'Все хорошо, колите, доктор, колите'
+      end
+    end
+  end
 
 
 end
